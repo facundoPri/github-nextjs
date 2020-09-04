e4b31beb69f4fa8aa771460256ea0ea297e29084
Facundo Prieto <facundo.prieto321@gmail.com>
2020-09-04 14:32:43 -0300

    set standard

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home () {
   return (
     <>
       <Head>
         <title>Github NextJs</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <h1>Hello World</h1>
     </>
