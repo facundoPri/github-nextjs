4440be915ed54200b5b16bc29afc8f2bf856ef47
Facundo Prieto <facundo.prieto321@gmail.com>
2020-09-04 19:39:43 -0300

    Form on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,48 @@
-import { Flex, Heading, Link } from '@chakra-ui/core'
+import {
+  Flex,
+  Heading,
+  Link,
+  Box,
+  Input,
+  Button,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  useToast,
+  FormErrorMessage
+} from '@chakra-ui/core'
 import { NextPage } from 'next'
 import NextLink from 'next/link'
+import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const IndexPage: NextPage = () => {
+  const { register, handleSubmit, watch, errors } = useForm()
+  const toast = useToast()
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
+  if (!!errors) {
+    console.log(errors.username)
+  }
+
   return (
     <Flex flexDir="column" alignItems="center" m={4}>
       <Heading as="h1" size="2xl">
-        Hello World
+        Users
       </Heading>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Flex>
+          <FormControl isInvalid={errors.username} mr="2">
+            <Input name="username" ref={register({ required: true })} />
+            <FormErrorMessage>{errors.username && 'Username is required'}</FormErrorMessage>
+          </FormControl>
+          <Button type="submit">Submit</Button>
+        </Flex>
+      </form>
+
       <NextLink href="/users" passHref>
         <Link>Go home</Link>
       </NextLink>
